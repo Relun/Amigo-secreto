2f78161c80afe7c4e88e8f93594f87012db8caa4
Claudia <claudia.araya.ro@gmail.com>
2025-08-22 17:10:59 -0400

    validación de que se ingrese un nombre válido y el campo no esté vacío, además se borra el input una vez que se presiona el botón añadir

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
-// Aquí deberás desarrollar la lógica para resolver el problema.
-
 let amigos = [];
 
 function agregarAmigo() { 
-    let amigo= document.getElementById('amigo').value;
-    amigos.push(amigo); 
-    console.log(amigos);
+    let amigo = document.getElementById('amigo').value;
 
+    if (amigo === ''){
+        alert('Por favor, ingresa un nombre válido');
+    }
+
+    amigos.push(amigo); 
+    document.getElementById('amigo').value = '';
+
+    //console.log(amigos);
+    //mostrarAmigos();
 }
+
+/*function mostrarAmigos() {
+    amigos.map(a => {
+        //ul id=listaAmigos
+        document.getElementById('listaAmigos').innerHTML=`<li> ${a} </li>`;
+        console.log(a);
+    });
+    
+}*/
